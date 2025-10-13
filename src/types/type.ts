ef66e4839d772ef7702d95629c424b26aa8d27cc
Sandra Linnea <sandra.b93@hotmail.com>
2025-10-13 14:12:02 +0200

    ad data and types for user

---
+++ src/types/type.ts
@@ -0,0 +1,20 @@
+interface NewUser {
+  user_id?: string;
+  name: string;
+  email: string;
+  password: string;
+  is_admin?: boolean;
+}
+
+interface User extends NewUser {
+  user_id: string;
+  password_hash: string;
+  created_at: string;
+}
+
+/* interface UserListQuery {
+  q?: string;
+  sort_by?: string;
+  offset?: number;
+  limit?: number;
+} */
