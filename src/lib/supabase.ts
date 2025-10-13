06ebd379fa3540ed9643be4142d686eaf93a3247
Sandra Linnea <sandra.b93@hotmail.com>
2025-10-13 14:12:17 +0200

    supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,19 @@
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv"
+dotenv.config();
+
+const _supabaseUrl = process.env.SUPABASE_URL;
+const _supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+if (!_supabaseUrl || !_supabaseAnonKey) {
+  throw new Error(
+    "Supabase not initalized add 'SUPABASE_URL' and 'SUPABASE_ANON_KEY' to enviroment variables"
+  );
+}
+
+export const supabase = createClient(_supabaseUrl, _supabaseAnonKey);
+
+const supabaseUrl = _supabaseUrl as string
+const supabaseAnonKey = _supabaseAnonKey as string
+
+export { supabaseUrl, supabaseAnonKey }
