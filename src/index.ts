816d624a1d564296573bfedf60c78cfa9c372c31
Sandra Linnea <sandra.b93@hotmail.com>
2025-10-13 14:38:30 +0200

    index.ts

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+// import { optionalAuth } from "./middleware/auth.js";
+// import { authApp } from "./routes/auth.js";
+// import userApp from "./routes/user.js";
+
+const app = new Hono( {
+  strict: false
+});
+
+const serverStartTime = Date.now()
+
+// app.use("*", optionalAuth)
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+// app.route ("/auth", authApp)
+// app.route("/courses", courseApp);
+// app.route("/students", studentApp);
+
+app.get("/health/", (c) => {
+  const now = Date.now()
+  const uptimeSeconds = Math.floor((now - serverStartTime) / 1000)
+
+  return c.json({
+    status: "ok",
+    message: "Service is healthy",
+    uptime:uptimeSeconds,
+    startedAt: new Date(serverStartTime).toISOString(),
+    timestamp: new Date().toISOString(),
+    version: "1.0.0",
+  })
+})
+
+app.onError((err, c) => {
+  if(err instanceof HTTPException){
+    console.log("managed risk error")
+    return err.getResponse()
+  }
+  console.log("unexpected error", err)
+  return c.json({ error: "Internal server error" }, 500);
+})
+
+serve(
+  {
+    fetch: app.fetch,
+    port: Number(process.env.HONO_PORT) || 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
